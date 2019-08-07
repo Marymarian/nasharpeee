ff45f44a5408102c84e2ab53d6291f67079c8e38
Mary <52426304+Marymarian@users.noreply.github.com>
2019-08-07 14:39:20 +0300

    Revert "Добавили классы работы с шаблонами, сохранение, отображение."

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.cbTemplates = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.TB1 = new System.Windows.Forms.TextBox();
             this.TB2 = new System.Windows.Forms.TextBox();
@@ -41,31 +41,24 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.YO = new System.Windows.Forms.Button();
-            this.rtbTemplate = new System.Windows.Forms.RichTextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.btInsertTemplate = new System.Windows.Forms.Button();
-            this.tbTemplateName = new System.Windows.Forms.TextBox();
-            this.tbTemplateSeparator = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // cbTemplates
+            // comboBox1
             // 
-            this.cbTemplates.FormattingEnabled = true;
-            this.cbTemplates.Location = new System.Drawing.Point(50, 63);
-            this.cbTemplates.Name = "cbTemplates";
-            this.cbTemplates.Size = new System.Drawing.Size(245, 21);
-            this.cbTemplates.TabIndex = 0;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(50, 63);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(245, 21);
+            this.comboBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 47);
+            this.label1.Location = new System.Drawing.Point(54, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Список шаблонов";
+            this.label1.Text = "label1";
             // 
             // TB1
             // 
@@ -157,77 +150,11 @@
             this.YO.UseVisualStyleBackColor = true;
             this.YO.Click += new System.EventHandler(this.YO_Click);
             // 
-            // rtbTemplate
-            // 
-            this.rtbTemplate.Location = new System.Drawing.Point(50, 198);
-            this.rtbTemplate.Name = "rtbTemplate";
-            this.rtbTemplate.Size = new System.Drawing.Size(344, 88);
-            this.rtbTemplate.TabIndex = 13;
-            this.rtbTemplate.Text = "";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(47, 182);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(46, 13);
-            this.label7.TabIndex = 14;
-            this.label7.Text = "Шаблон";
-            // 
-            // btInsertTemplate
-            // 
-            this.btInsertTemplate.Location = new System.Drawing.Point(319, 288);
-            this.btInsertTemplate.Name = "btInsertTemplate";
-            this.btInsertTemplate.Size = new System.Drawing.Size(75, 23);
-            this.btInsertTemplate.TabIndex = 15;
-            this.btInsertTemplate.Text = "Добавить";
-            this.btInsertTemplate.UseVisualStyleBackColor = true;
-            this.btInsertTemplate.Click += new System.EventHandler(this.btInsertTemplate_Click);
-            // 
-            // tbTemplateName
-            // 
-            this.tbTemplateName.Location = new System.Drawing.Point(50, 159);
-            this.tbTemplateName.Name = "tbTemplateName";
-            this.tbTemplateName.Size = new System.Drawing.Size(124, 20);
-            this.tbTemplateName.TabIndex = 16;
-            // 
-            // tbTemplateSeparator
-            // 
-            this.tbTemplateSeparator.Location = new System.Drawing.Point(195, 159);
-            this.tbTemplateSeparator.Name = "tbTemplateSeparator";
-            this.tbTemplateSeparator.Size = new System.Drawing.Size(100, 20);
-            this.tbTemplateSeparator.TabIndex = 17;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(47, 143);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(104, 13);
-            this.label8.TabIndex = 18;
-            this.label8.Text = "Название шаблона";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(192, 143);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(73, 13);
-            this.label9.TabIndex = 18;
-            this.label9.Text = "Разделитель";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.tbTemplateSeparator);
-            this.Controls.Add(this.tbTemplateName);
-            this.Controls.Add(this.btInsertTemplate);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.rtbTemplate);
             this.Controls.Add(this.YO);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -240,11 +167,9 @@
             this.Controls.Add(this.TB2);
             this.Controls.Add(this.TB1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.cbTemplates);
+            this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -252,7 +177,7 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox cbTemplates;
+        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox TB1;
         private System.Windows.Forms.TextBox TB2;
@@ -265,13 +190,6 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button YO;
-        private System.Windows.Forms.RichTextBox rtbTemplate;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Button btInsertTemplate;
-        private System.Windows.Forms.TextBox tbTemplateName;
-        private System.Windows.Forms.TextBox tbTemplateSeparator;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -17,10 +17,6 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        KeyBoardHook Hooker = new KeyBoardHook();
-        Rules rules = new Rules();
-
-
         private void YO_Click(object sender, EventArgs e)
         {
             //Если в буфере обмен содержится текст
@@ -28,11 +24,10 @@ namespace WindowsFormsApp1
             {
                 //Извлекаем (точнее копируем) его и сохраняем в переменную
                 string strT = Clipboard.GetText();
-                string[] PromArr = strT.Split('>');
-                //do
+                do
                     
-                //    TB1.Text = [0];
-                //while (strT == "\n");
+                    TB1.Text = strT.Split('>')[0];
+                while (strT = '\n');
                 ////Выводим показываем сообщение с текстом, скопированным из буфера обмена
                 //MessageBox.Show(this, strT, "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Asterisk); 
             }
@@ -42,36 +37,6 @@ namespace WindowsFormsApp1
                 //Выводим сообщение о том, что в буфере обмена нет текста
                 MessageBox.Show(this, "В буфере обмена нет текста", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            Clipboard.SetText ("Пётр>Сергеевич>Валентир>01.12.89>М>Северная верфь");
-            rules = Rules.Deserialise;
-            cbTemplates.Items.AddRange(rules.templates.ToArray());
-            Hooker.SetHook();
-        }
-
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            Hooker.Unhook();
-        }
-
-        private void btInsertTemplate_Click(object sender, EventArgs e)
-        {
-            if(tbTemplateName.Text.Trim().Length>0 && tbTemplateSeparator.Text.Length>0)
-                if (rtbTemplate.Text.Trim().Length > 0)
-                {
-                    Template template = new Template();
-                    template.Name = tbTemplateName.Text.Trim();
-                    template.Separator = tbTemplateSeparator.Text;
-                    template.TemplateRow = rtbTemplate.Text;
-                    string[] Parts = template.TemplateRow.Split(new string[] {template.Separator},StringSplitOptions.None);
-                    template.Rule = new List<string>(Parts);
-                    rules.templates.Add(template);
-                    cbTemplates.Items.Add(template);
-                    Rules.Serialise(rules);
-                }
         }
     }
 }
